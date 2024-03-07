514ecd579ca8f8cee76da3780bc7f4e7689ebf2b
feassis <felipe.assis.2002@gmail.com>
2024-03-07 18:12:12 -0300

    golems 3 and 4 attacks

--- Characters/enemy/Golem 2/Golem2ChargeAttackDetection.gd
+++ Characters/enemy/Golem 2/Golem2ChargeAttackDetection.gd
@@ -3,18 +3,25 @@ class_name Golem2AttackDetection
 
 var player:Player = null
 
+var isAttacking : bool = false
+var hasAttack: bool = false
 
+func _process(delta):
+	if isAttacking && !hasAttack:
+		ChargeAttack()
 
 func ChargeAttack():
 	if player == null:
 		return
 	
+	hasAttack = true
 	player.HalfLife()
 
 
 func _on_body_entered(body):
 	if body is Player:
 		player = (body as Player)
+		
 
 
 func _on_body_exited(body):
